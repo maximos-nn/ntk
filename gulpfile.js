3c4972c1277bd9237f0a922ba175a934319dc662
maximos-nn <52293321+maximos-nn@users.noreply.github.com>
2021-04-28 16:05:34 +0300

    minimal enviroment

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,27 @@
 import gulp from 'gulp';
+import sass from 'gulp-sass';
+import compiler from 'node-sass';
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+import browserSync from 'browser-sync';
+
+sass.compiler = compiler;
+const server = browserSync.create();
 
 const {src, dest, series, parallel, watch} = gulp;
 
-const defaultTask = (cb) => {
-  cb();
-}
+const css = () => src('source/sass/style.scss', {sourcemaps: true})
+  .pipe(sass().on('error', sass.logError))
+  .pipe(postcss([autoprefixer()]))
+  .pipe(dest('source/css', {sourcemaps: '.'}));
 
-export default defaultTask;
+export default () => {
+  server.init({
+    server: "source/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+  watch('source/sass/**/*.{scss,sass}', css);
+};
